52c37388b59a83c52cc71a5f8684be3075aadf80
fatemeh <fatimanoori998@gmail.com>
2021-04-20 13:52:23 +0430

    init project

---
+++ app/src/main/java/ir/team_x/ariana/driver/activity/MainActivity.kt
@@ -0,0 +1,12 @@
+package ir.team_x.ariana.driver.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ir.team_x.ariana.driver.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
