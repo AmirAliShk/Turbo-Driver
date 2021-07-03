485d3f2cb066d53508d098543551a52069e90e3a
fatemeh <fatimanoori1998@gmail.com>
2021-07-03 18:31:39 +0430

    add finish model

---
+++ app/src/main/java/ir/team_x/ariana/driver/model/FinishedModel.kt
@@ -0,0 +1,17 @@
+package ir.team_x.ariana.driver.model
+
+data class FinishedModel(
+    var id:Int,
+    var customerId:Int,
+    var sourceAddressId:Int,
+    var destinationAddressId:Int,
+    var saveDate:String,
+    var status:Int,
+    var finishDate:String,
+    var acceptDate:String,
+    var price:String,
+    var statusDes:String,
+    var sourceAddress:String,
+    var destinationAddress:String,
+    var statusColor:String
+)
