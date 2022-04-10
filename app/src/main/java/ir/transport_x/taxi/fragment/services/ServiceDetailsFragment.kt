831ac37620b1989cecc73aef36d3bda20c4ae52f
khakinezhad <k.sniper2009@gmail.com>
2022-04-10 17:04:24 +0430

    add voice to ServiceDetailFragment with singelton

--- app/src/main/java/ir/transport_x/taxi/fragment/services/ServiceDetailsFragment.kt
+++ app/src/main/java/ir/transport_x/taxi/fragment/services/ServiceDetailsFragment.kt
@@ -1,10 +1,12 @@
 package ir.transport_x.taxi.fragment.services
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.downloader.Progress
 import ir.transport_x.taxi.app.EndPoint
 import ir.transport_x.taxi.app.MyApplication
 import ir.transport_x.taxi.databinding.FragmentServiceDetailsBinding
@@ -14,13 +16,11 @@ import ir.transport_x.taxi.dialog.GeneralDialog
 import ir.transport_x.taxi.dialog.GetPriceDialog
 import ir.transport_x.taxi.model.ServiceDataModel
 import ir.transport_x.taxi.okHttp.RequestHelper
-import ir.transport_x.taxi.utils.DateHelper
-import ir.transport_x.taxi.utils.FragmentHelper
-import ir.transport_x.taxi.utils.StringHelper
-import ir.transport_x.taxi.utils.TypeFaceUtilJava
+import ir.transport_x.taxi.utils.*
 import ir.transport_x.taxi.webServices.UpdateCharge
 import org.json.JSONArray
 import org.json.JSONObject
+import java.util.*
 
 class ServiceDetailsFragment(
     serviceModel: ServiceDataModel,
@@ -32,6 +32,7 @@ class ServiceDetailsFragment(
 
     private val serviceModel = serviceModel
     private lateinit var binding: FragmentServiceDetailsBinding
+    var lastTime: Long = 0
 
     interface CancelServiceListener {
         fun onCanceled(isCancel: Boolean)
@@ -107,9 +108,11 @@ class ServiceDetailsFragment(
 
         binding.txtCustomerName.text = serviceModel.customerName
         binding.txtOriginAddress.text = StringHelper.toPersianDigits(serviceModel.sourceAddress)
-        binding.txtPrice.text =  "${
+        binding.txtPrice.text = "${
             StringHelper.toPersianDigits(
-                StringHelper.setComma(serviceModel.price))} تومان "
+                StringHelper.setComma(serviceModel.price)
+            )
+        } تومان "
         binding.txtFirstDestAddress.text = StringHelper.toPersianDigits(
             JSONArray(serviceModel.destinationAddress).getJSONObject(0).getString("address")
         )
@@ -152,9 +155,9 @@ class ServiceDetailsFragment(
             CallDialog().show(serviceModel.phoneNumber, serviceModel.mobile)
         }
         binding.txtFinish.setOnClickListener {
-            if(MyApplication.prefManager.pricing==1){
+            if (MyApplication.prefManager.pricing == 1) {
                 bill(serviceModel.id, serviceModel.priceService)
-            }else{
+            } else {
                 GetPriceDialog().show(serviceModel.id,
                     object : GetPriceDialog.FinishServiceListener {
                         override fun onFinishService(isFinish: Boolean) {
@@ -163,6 +166,78 @@ class ServiceDetailsFragment(
 
                     })
             }
+        }
+
+        binding.imgPlayVoice.setOnClickListener {
+            val voiceName = "voipId.mp3"
+            VoiceHelper.getInstance()
+                .autoplay("", voiceName, "voipId", object : VoiceHelper.OnVoiceListener {
+                    override fun onFileExist() {
+                    }
+
+                    override fun onStartDownload() {
+                        binding.vfDownloadOrPlay.displayedChild = 1
+                    }
+
+                    override fun onProgressDownload(progress: Progress?) {
+                        val percent =
+                            (progress!!.currentBytes / progress!!.totalBytes.toDouble() * 100).toInt()
+                        Log.i("ServiceDetailsFragment", "onProgress: $percent")
+
+                        binding.progressBar.progress = percent
+                        if (Calendar.getInstance().timeInMillis - lastTime > 500) {
+                            binding.textProgress.text = "$percent %"
+                            lastTime = Calendar.getInstance().timeInMillis
+                        }
+                    }
+
+                    override fun onDownloadCompleted() {
+                        binding.vfDownloadOrPlay.displayedChild = 0
+                        binding.vfPlayPause.displayedChild = 1
+                    }
+
+                    override fun onDownloadError() {
+                    }
+
+                    override fun onDownload401Error() {
+                    }
+
+                    override fun onDownload404Error() {
+                        binding.vfDownloadOrPlay.displayedChild = 2
+                    }
+
+                    override fun onDuringInit() {
+//                        binding.skbTimer.setProgress(0f)
+                    }
+
+                    override fun onEndOfInit(maxDuration: Int) {
+                        binding.skbTimer.max = maxDuration.toFloat()
+                    }
+
+                    override fun onPlayVoice() {
+                        binding.vfPlayPause.displayedChild = 1
+                    }
+
+                    override fun onTimerTask(currentDuration: Int) {
+                        binding.skbTimer.setProgress(currentDuration.toFloat())
+                        val timeRemaining: Int = currentDuration / 1000
+                        val strTimeRemaining = String.format(
+                            Locale("en_US"),
+                            "%02d:%02d",
+                            timeRemaining / 60,
+                            timeRemaining % 60
+                        )
+                        binding.txtTime.text = strTimeRemaining
+                    }
+
+                    override fun onPauseVoice() {
+                        binding.vfPlayPause.displayedChild = 0
+                    }
+
+                    override fun onVoipIdEqual0() {
+                        binding.vfDownloadOrPlay.displayedChild = 2
+                    }
+                })
         }
 
         return binding.root
